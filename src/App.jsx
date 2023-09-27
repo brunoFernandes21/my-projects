1ddcd3f9c55ed787fcd25b699701bba0eadcd46b
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-09-27 18:20:35 +0100

    add img to all the projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Home from "./pages/Home";
-function App() {
+import ParticlesBg from "particles-bg";
 
+function App() {
   return (
     <div className="h-full lg:h-screen py-20">
+      <ParticlesBg color="#f4f4f4" num={200} type="cobweb" bg={true} />
       <div className="flex justify-center mb-20">
-        <h1 className="bg-black font-bold text-2xl text-white py-2 px-3 rounded-full shadow-md shadow-white transition-all duration-500 ease-in-out hover:scale-110">Bruno Fernandes' Projects</h1>
+        <h1 className="bg-black font-bold text-2xl text-white py-2 px-3 rounded-full shadow-md shadow-white transition-all duration-500 ease-in-out hover:scale-110">
+          Bruno Fernandes' Projects
+        </h1>
       </div>
       <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,20 +1,45 @@
-import ProjectCard from "./ProjectCard"
+import ProjectCard from "./ProjectCard";
 const myProjects = [
-  {id: 1, image: "/northcoders07.png", title: "Northcoders News", url: "https://www.bfnc-news.com"},
-  {id: 2, image: "/northcoders07.png", title: "Books-Buzz", url: "https://books-buzz-app.netlify.app/landing-page"},
-  {id: 3, image: "/northcoders07.png", title: "Developer Portfolio", url: "https://bruno-dev-portfolio.netlify.app"},
-  {id: 4, image: "/northcoders07.png", title: "Total Fitness"},
-  {id: 5, image: "/northcoders07.png", title: "Monster Friends", url: "https://brunofernandes21.github.io/monsterfriends/"},
-  {id: 6, image: "/northcoders07.png", title: "Cloud Todo", url: "https://bruno-dev-portfolio.netlify.app"},
-]
+  {
+    id: 1,
+    image: "/northcoders07.png",
+    title: "Northcoders News",
+    url: "https://www.bfnc-news.com",
+  },
+  {
+    id: 2,
+    image: "/books-buzz1.png",
+    title: "Books-Buzz",
+    url: "https://books-buzz-app.netlify.app/landing-page",
+  },
+  {
+    id: 3,
+    image: "/portfolio.png",
+    title: "Developer Portfolio",
+    url: "https://bruno-dev-portfolio.netlify.app",
+  },
+  { id: 4, image: "/fitness1.png", title: "Total Fitness" },
+  {
+    id: 5,
+    image: "/monsters.png",
+    title: "Monster Friends",
+    url: "https://brunofernandes21.github.io/monsterfriends/",
+  },
+  { id: 6, image: "/cloudTodo.png", title: "Cloud Todo" },
+];
 const ProjectList = () => {
   return (
     <div className="flex flex-wrap justify-center gap-4 md:gap-6">
-        {myProjects.map((project) => (
-            <ProjectCard key={project.id} image={project.image} title={project.title} url={project.url}/>
-        ))}
+      {myProjects.map((project) => (
+        <ProjectCard
+          key={project.id}
+          image={project.image}
+          title={project.title}
+          url={project.url}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProjectList
+export default ProjectList;
